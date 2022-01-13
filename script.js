cd764adb3ef0b55f5a4478219321cdeb67e2cbd4
miuhuru <zermoun.yasmine@gmail.com>
2022-01-13 20:13:23 +0100

    trying the logic with some objects

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const Player = (name, choice, score) => {
+  const getName = () => name
+  const getChoice = () => choice
+  const getScore = () => score 
+  return {name, choice, score}
+  }
+  
+  const Gameboard = (gameboard, Player) => {
+  let gameboard = []
+  }
+  
+  
