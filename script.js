148bb9fbcd86e809d3193639ce398d853e436bbd
miuhuru <zermoun.yasmine@gmail.com>
2022-01-15 22:22:04 +0100

    testing some functions to start putting Xs and Os on the board

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-"use strict";
 
-const Player = (name, choice, score) => {
-  const getName = () => name
-  const getChoice = () => choice
-  const getScore = () => score 
-  return {getName, getChoice, getScore}
-  }
-  
-const game
-  
-  
+
+
+const Gameboard = (function () {
+let gameBoard = []
+})();
+ 
+const Player = (name) => {
+    const getName = () => name
+    return {name, getName}
+}
+
+/// first step is to get my Xs and Os to appear on the grid
+
+const displayController = (function () {
+    return {     
+        
+    }
+})();
+    
+
+const Game = () => {
+    const flow = () => {
+        document.querySelector(".X").addEventListener("click",
+            () => {
+                document.querySelectorAll(".box").forEach(box =>
+                    box.innerText = "X"
+                )
+            })
+    }
+    flow()
+}
+Game()
+
