a9cda018169e60254e744b9c48c020f197d938ea
miuhuru <zermoun.yasmine@gmail.com>
2022-01-17 23:20:35 +0100

    added feature that disables the grid if the player hasnt chosen a letter yet

--- script.js
+++ script.js
@@ -18,14 +18,15 @@ const displayControler = (function () { // module pattern
   let moves = 0;
   let nextTurn 
 
-  function choosePlayer() {
-    if (e.target.id == "X") {
+  function choosePlayer (event) {
+    if (event.target.innerText == "X") {
       nextTurn = "X"
-    } else if (e.target.id == "O") {
+      console.log(nextTurn)
+    } else if (event.target.innerText == "O") {
       nextTurn = "O"
+      console.log(this.innerText, nextTurn)
     }
   }
-
   function changeTurn(){
     if(nextTurn == 'X'){
          nextTurn = 'O';
@@ -35,14 +36,21 @@ const displayControler = (function () { // module pattern
 }
   document.querySelectorAll(".box").forEach(elem =>
     elem.addEventListener("click", (e) => {
-      if (e.target.innerText == "") {
-        e.target.innerText = nextTurn
-        changeTurn()
-        moves+
+      if (!nextTurn) {
+        document.querySelectorAll(".box").forEach(box => box.classList.add("rednotice"))
+        document.querySelector(".choosers").innerText = "Choose X or O"
+        document.querySelector(".choosers").style.color = "hsl(8, 89%, 50%, 0.9)"
+      } else if (nextTurn) {
+        document.querySelectorAll(".box").forEach(box => box.classList.remove("rednotice"))
+        if (e.target.innerText == "") {
+          e.target.innerText = nextTurn
+          changeTurn()
+          moves+
         console.log(moves++)
-      } else {
+        } 
       }
-    }))
+      }))
+    
   return {
     choosePlayer
   };
