4bd97de32299bbc3490a8a9778bf72382ae54749
miuhuru <zermoun.yasmine@gmail.com>
2022-01-17 22:34:31 +0100

    implemented change turns function

--- script.js
+++ script.js
@@ -6,63 +6,45 @@ const Gameboard = (function () {
   };
 })();
 
-const Player = (name) => {  // factory pattern
+const Player = (name) => {
+  // factory pattern
   return { name };
 };
 
 const player1 = Player("player1");
 const player2 = Player("player2");
 
-const displayControler = (function () {   // module pattern
-    let moves = 0;
-    let count = 0
-    let choice
-    let buttons = document.querySelector(".buttonChoices")
-    let playerTurn
-    let start = document.querySelector(".start")
+const displayControler = (function () { // module pattern
+  let moves = 0;
+  let nextTurn 
 
-    let startGame = function () {
-        start.addEventListener("click", () => {
-            playerChoice()
-            if ()
-        }
-        )
+  function choosePlayer() {
+    if (e.target.id == "X") {
+      nextTurn = "X"
+    } else if (e.target.id == "O") {
+      nextTurn = "O"
     }
-    let playerChoice = function () {
-        buttons.addEventListener("click", (e) => {
-            if (e.target.innerText === "X" && Gameboard.gameBoard.slice(-1) !== "X") {
-                choice = "X"
-                playerTurn = player1
-                Gameboard.gameBoard.push(choice)
-                console.log(Gameboard.gameBoard)
-            } else if (e.target.innerText === "O" && Gameboard.gameBoard.slice(-1) !== "O") {
-                choice = "O"
-                playerTurn = player2
-                Gameboard.gameBoard.push(choice)
-                console.log(Gameboard.gameBoard)
-            }
-        }
-        )
+  }
+
+  function changeTurn(){
+    if(nextTurn == 'X'){
+         nextTurn = 'O';
+    } else {
+         nextTurn = 'X';
     }
-    let playerMoves = function () {
-            document.querySelectorAll(".box").forEach(box =>
-                box.addEventListener("click", (e) => {
-                    if (playerTurn = player1) {
-                        box.innerText = choice
-                        console.log(choice)
-                } else if (playerTurn = player2) {
-                    box.innerText = choice
-                }              
-            }))
-    }
+}
+  document.querySelectorAll(".box").forEach(elem =>
+    elem.addEventListener("click", (e) => {
+      if (e.target.innerText == "") {
+        e.target.innerText = nextTurn
+        changeTurn()
+        moves+
+        console.log(moves++)
+      } else {
+      }
+    }))
   return {
-    moves, count, buttons, playerMoves, startGame
+    choosePlayer
   };
 })();
 
-displayControler.startGame();
-displayControler.playerMoves();
-
-    
-  
-  
