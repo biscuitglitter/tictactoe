d3228e70da7cae654eb3251900b222da080e1baa
miuhuru <zermoun.yasmine@gmail.com>
2022-01-18 14:57:45 +0100

    added a function that populates the gameBoard array with each move

--- script.js
+++ script.js
@@ -6,57 +6,60 @@ const Gameboard = (function () {
   };
 })();
 
-const Player = (name) => {
-  // factory pattern
+const Player = (name) => { 
   return { name };
 };
 
 const player1 = Player("player1");
 const player2 = Player("player2");
 
-const displayControler = (function () { // module pattern
+const displayControler = (function () {
   let moves = 0;
-  let nextTurn 
+  let nextTurn
 
-  function changeTurn(){
-    if(nextTurn == "X"){
-         nextTurn = "O";
-    } else {
-         nextTurn = "X";
-    }
-  }
   function chooseLetter() {
+    document.querySelectorAll(".box").forEach(box => box.classList.remove("rednotice"))
     document.querySelectorAll(".buttonChoices").forEach(button => button.addEventListener("click", (e) => {
       if (e.target.innerText === "X") {
-        nextTurn = "X"; console.log("nextTurn " + nextTurn)
+        nextTurn = "X"; 
+        console.log("it got fired!")
       } else if (e.target.innerText === "O") {
         nextTurn = "O"
-        console.log("nextTurn " + nextTurn)
       }
     }))
   }
-  document.querySelectorAll(".box").forEach(elem =>
-    elem.addEventListener("click", (e) => {
-      if (!nextTurn) {
-        document.querySelectorAll(".box").forEach(box => box.classList.add("rednotice"))
-        document.querySelector(".choosers").innerText = "You have to choose a letter"
-        document.querySelector(".choosers").style.color = "hsl(8, 89%, 50%, 0.9)"
-      } else if (nextTurn) {
-        document.querySelectorAll(".box").forEach(box => box.classList.remove("rednotice"))
-        if (e.target.innerText == "") {
-          e.target.innerText = nextTurn
-          changeTurn()
-          moves+
-        console.log(moves++)
-        } 
-      }
-    }))
+  function playerMarks() {
+    document.querySelectorAll(".box").forEach(elem =>
+      elem.addEventListener("click", (e) => {
+        if (nextTurn) {
+          if (e.target.innerText == "") {
+            e.target.innerText = nextTurn
+            changeTurn();
+            Gameboard.gameBoard.push(nextTurn)
+            console.log(Gameboard.gameBoard)
+          }
+        } else if (!nextTurn) {
+          document.querySelectorAll(".box").forEach(box => box.classList.add("rednotice"))
+          document.querySelector(".choosers").innerText = "You have to choose a letter"
+          document.querySelector(".choosers").style.color = "hsl(8, 89%, 50%, 0.9)"
+          chooseLetter()
+        }
+      }))
+  }
 
-    
+  function changeTurn() {
+    if (nextTurn == "X") {
+      nextTurn = "O";
+    } else {
+      nextTurn = "X";
+    }
+  }
+  
   return {
-    chooseLetter
+    chooseLetter, playerMarks
   };
 
 })();
 
 displayControler.chooseLetter()
+displayControler.playerMarks()
