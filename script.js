821d8bb2b053d0aaf7da3b9c8d1f537877725c65
miuhuru <zermoun.yasmine@gmail.com>
2022-01-21 18:50:39 +0100

    added a Game module to control the flow of the game

--- script.js
+++ script.js
@@ -10,40 +10,49 @@ const Gameboard = (function () {
 const Player = {
   name: "",
   letter: "",
-}
+};
 
 const displayControler = (function () {
-  let nextTurn
-  
+  let nextTurn;
+
   function chooseLetter() {
-    document.querySelectorAll(".box").forEach(box => box.classList.remove("rednotice"))
-    document.querySelectorAll(".buttons").forEach(button => button.addEventListener("click", (e) => {
-      if (e.target.innerText === "X") {
-        nextTurn = "X";
-      } else if (e.target.innerText === "O") {
-        nextTurn = "O"
-      }
-    }))
+    document
+      .querySelectorAll(".box")
+      .forEach((box) => box.classList.remove("rednotice"));
+    document.querySelectorAll(".buttons").forEach((button) =>
+      button.addEventListener("click", (e) => {
+        if (e.target.innerText === "X") {
+          nextTurn = "X";
+        } else if (e.target.innerText === "O") {
+          nextTurn = "O";
+        }
+      })
+    );
   }
 
   function playerMarks() {
-    document.querySelectorAll(".box").forEach(elem =>
+    document.querySelectorAll(".box").forEach((elem) =>
       elem.addEventListener("click", (e) => {
         if (nextTurn) {
           if (e.target.innerText == "") {
-            e.target.innerText = nextTurn
-            Gameboard.gameBoard.push(nextTurn)
-            console.log(Gameboard.gameBoard)
+            e.target.innerText = nextTurn;
+            Gameboard.gameBoard.push(nextTurn);
+            console.log(Gameboard.gameBoard);
             changeTurn();
-            winningCombos();
+            Game.winningCombos();
           }
         } else if (!nextTurn) {
-          document.querySelectorAll(".box").forEach(box => box.classList.add("rednotice"))
-          document.querySelector(".choosers").innerText = "You have to choose a letter"
-          document.querySelector(".choosers").style.color = "hsl(8, 89%, 50%, 0.9)"
-          chooseLetter()
+          document
+            .querySelectorAll(".box")
+            .forEach((box) => box.classList.add("rednotice"));
+          document.querySelector(".choosers").innerText =
+            "You have to choose a letter";
+          document.querySelector(".choosers").style.color =
+            "hsl(8, 89%, 50%, 0.9)";
+          chooseLetter();
         }
-      }))
+      })
+    );
   }
 
   function changeTurn() {
@@ -53,36 +62,90 @@ const displayControler = (function () {
       nextTurn = "X";
     }
   }
-
-  function winningCombos() {   
-    if (document.getElementById("1").innerText === document.getElementById("2").innerText && document.getElementById("2").innerText === document.getElementById("3").innerText && document.getElementById("3").innerText !== "") {
-        console.log("WOO!")
-        chooseWinner()
-      } else if (document.getElementById("4").innerText === document.getElementById("5").innerText && document.getElementById("5").innerText === document.getElementById("6").innerText && document.getElementById("6").innerText !== "") {
-        document.querySelector(".choosers").innerText = "you win!"
-      } else if (document.getElementById("7").innerText === document.getElementById("8").innerText && document.getElementById("8").innerText === document.getElementById("9").innerText && document.getElementById("9").innerText !== "") {
-        document.querySelector(".choosers").innerText = "you win!"
-      } else if (document.getElementById("1").innerText === document.getElementById("4").innerText && document.getElementById("4").innerText === document.getElementById("7").innerText && document.getElementById("7").innerText !== "") {
-        document.querySelector(".choosers").innerText = "you win!"
-      } else if (document.getElementById("2").innerText === document.getElementById("5").innerText && document.getElementById("5").innerText === document.getElementById("8").innerText && document.getElementById("8").innerText !== "") {
-        document.querySelector(".choosers").innerText = "you win!"
-      } else if (document.getElementById("3").innerText === document.getElementById("6").innerText && document.getElementById("6").innerText === document.getElementById("9").innerText && document.getElementById("9").innerText !== "") {
-        document.querySelector(".choosers").innerText = "you win!"
-      } else if (document.getElementById("1").innerText === document.getElementById("5").innerText && document.getElementById("5").innerText === document.getElementById("9").innerText && document.getElementById("9").innerText !== "") {
-        document.querySelector(".choosers").innerText = "you win!"
-      } else if (document.getElementById("3").innerText === document.getElementById("5").innerText && document.getElementById("5").innerText === document.getElementById("7").innerText && document.getElementById("7").innerText !== "") {
-        document.querySelector(".choosers").innerText = "you win!"
-    } else {
-        return
-      }
-    
-      }
-  
-   return {
-    chooseLetter, playerMarks, winningCombos
+  return {
+    chooseLetter,
+    playerMarks,
+    nextTurn,
+    changeTurn,
   };
-
 })();
 
-displayControler.chooseLetter()
-displayControler.playerMarks()
+displayControler.chooseLetter();
+displayControler.playerMarks();
+
+const Game = (function () {
+  let one = document.getElementById("1");
+  let two = document.getElementById("2");
+  let three = document.getElementById("3");
+  let four = document.getElementById("4");
+  let five = document.getElementById("5");
+  let six = document.getElementById("6");
+  let seven = document.getElementById("7");
+  let eight = document.getElementById("8");
+  let nine = document.getElementById("9");
+
+  function winningCombos() {
+    if (
+      one.innerText === two.innerText &&
+      two.innerText === three.innerText &&
+      three.innerText !== ""
+    ) {
+      document.querySelector(".choosers").innerText = "you win!";
+      console.log("you win");
+    } else if (
+      four.innerText === five.innerText &&
+      five.innerText === six.innerText &&
+      six.innerText !== ""
+    ) {
+      document.querySelector(".choosers").innerText = "you win!";
+      console.log("you win");
+    } else if (
+      seven.innerText === eight.innerText &&
+      eight.innerText === nine.innerText &&
+      nine.innerText !== ""
+    ) {
+      document.querySelector(".choosers").innerText = "you win!";
+      console.log("you win");
+    } else if (
+      one.innerText === four.innerText &&
+      four.innerText === seven.innerText &&
+      seven.innerText !== ""
+    ) {
+      document.querySelector(".choosers").innerText = "you win!";
+      console.log("you win");
+    } else if (
+      two.innerText === five.innerText &&
+      five.innerText === eight.innerText &&
+      eight.innerText !== ""
+    ) {
+      document.querySelector(".choosers").innerText = "you win!";
+      console.log("you win");
+    } else if (
+      three.innerText === six.innerText &&
+      six.innerText === nine.innerText &&
+      nine.innerText !== ""
+    ) {
+      document.querySelector(".choosers").innerText = "you win!";
+      console.log("you win");
+    } else if (
+      one.innerText === five.innerText &&
+      five.innerText === nine.innerText &&
+      nine.innerText !== ""
+    ) {
+      document.querySelector(".choosers").innerText = "you win!";
+      console.log("you win");
+    } else if (
+      three.innerText === five.innerText &&
+      five.innerText === seven.innerText &&
+      seven.innerText !== ""
+    ) {
+      document.querySelector(".choosers").innerText = "you win!";
+      console.log("you win");
+    } else {
+      return;
+    }
+  }
+  return {
+    winningCombos,
+  };
+})();
